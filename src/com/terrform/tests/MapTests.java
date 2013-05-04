a62a9866f16e0dfe2808097b32b7a76a5f8c62ef
Ryan Wells <ryaanwells@googlemail.com>
2013-05-04 15:04:39 +0100

    Updated gitignore and checked junit

---
+++ src/com/terrform/tests/MapTests.java
@@ -0,0 +1,12 @@
+package com.terrform.tests;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MapTests {
+
+	@Test
+	public void ThisShouldFail(){
+		assertTrue(true);
+	}
+}
