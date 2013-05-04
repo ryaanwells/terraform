b4d67ebc4cfb37b30e7b3da210755e71b332f9fc
Blair Archibald <mrblairarchibald@gmail.com>
2013-05-04 14:44:55 +0100

    Basic game functionality added

---
+++ src/com/terrform/desktop/DesktopMain.java
@@ -0,0 +1,11 @@
+package com.terrform.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.terraform.main.Terraform;
+
+public class DesktopMain {
+	public static void main (String[] args) {
+        new LwjglApplication(new Terraform(), "Terraform", 480, 320, false);
+}
+
+}
